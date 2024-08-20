a1f355d80b7d9afaf8c351c33ee9f6951be7315f
LanCorC <lanc3.corcega@gmail.com>
2024-08-20 18:18:29 +1200

    Section 23.404(Java Networking with Channels, Reactive Programming, and Virtual Threads: Introduction to WebSocket): Simple demonstration of WebSocket code, as server (third party) or client (java.net.http);

---
+++ src/dev/rca/WebSocketClient.java
@@ -0,0 +1,20 @@
+package dev.rca;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.WebSocket;
+
+public class WebSocketClient {
+    public static void main(String[] args) throws URISyntaxException {
+        HttpClient client = HttpClient.newHttpClient();
+        WebSocket webSocket = client.newWebSocketBuilder()
+                .buildAsync(new URI("ws://localhost:8080"),
+                        new WebSocket.Listener() {
+//                            @Override
+//                            public void onOpen(WebSocket webSocket) {
+//                                WebSocket.Listener.super.onOpen(webSocket);
+//                            }
+                        }).join();
+    }
+}
