c51bf667c10ff6df8c694ed8d63c1f34aa579e14
LanCorC <lanc3.corcega@gmail.com>
2024-08-20 22:54:01 +1200

    Section 23.405(Java Networking with Channels, Reactive Programming, and Virtual Threads: Writing a WebSocket Chat Application): Adding on to the basic classes to demonstrate a simple live chat application using WebSockets.

--- src/dev/rca/WebSocketClient.java
+++ src/dev/rca/WebSocketClient.java
@@ -4,17 +4,44 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
+import java.util.Scanner;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
 public class WebSocketClient {
     public static void main(String[] args) throws URISyntaxException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name to join chat: ");
+        String name = scanner.nextLine();
+
         HttpClient client = HttpClient.newHttpClient();
         WebSocket webSocket = client.newWebSocketBuilder()
-                .buildAsync(new URI("ws://localhost:8080"),
+                .buildAsync(new URI("ws://localhost:8080?name=%s"
+                                .formatted(name)),
                         new WebSocket.Listener() {
-//                            @Override
-//                            public void onOpen(WebSocket webSocket) {
-//                                WebSocket.Listener.super.onOpen(webSocket);
-//                            }
+                            @Override
+                            public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+                                System.out.println(data);
+                                return WebSocket.Listener.super.onText(webSocket, data, last);
+                            }
+
                         }).join();
+
+        while(true) {
+            String input = scanner.nextLine();
+            if("bye".equalsIgnoreCase(input)) {
+                try {
+                    webSocket.sendClose(WebSocket.NORMAL_CLOSURE,
+                            "User left normally").get();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            } else {
+                webSocket.sendText(input,true);
+                //'true' to indicate this is the LAST PART of the msg
+                //used for when sending LARGE data, split up
+            }
+        }
     }
 }
